5be69c65822c19453b22c1d3f7618c3d30f067b1
Themiya <themiyakaluarachchi@gmail.com>
2025-04-21 02:02:24 +0530

    1st commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+      displayCalculation();
+
+      function updateCalculation(value)
+      {
+        calculation += value;
+        //console.log(calculation);
+        displayCalculation();
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function saveCalculation() 
+      {
+        localStorage.setItem('calculation', calculation);  
+      }
+
+      function displayCalculation() 
+      {
+        document.querySelector('.js-calculation').innerHTML = calculation;  
+      }
